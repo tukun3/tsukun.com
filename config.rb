9f39da749ec510748e0c52829780df55409e4000
Nozomu Tsukuda <zenagakiku@gmail.com>
2017-07-23 19:43:39 +0900

    Fix layout

--- config.rb
+++ config.rb
@@ -53,6 +53,11 @@ set :js_dir, 'javascripts'
 
 set :images_dir, 'images'
 
+set :slim,
+    :format => :html5,
+    :sort_attrs => false,
+    :pretty => true
+
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
